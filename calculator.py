60df26b1c2dbf1b370c8f683c1d3aa69c984ae49
vagrant <vagrant@vagrant.vm>
2020-04-30 00:01:58 +0000

    Created While Loop, Input, and Add/Subtract

--- calculator.py
+++ calculator.py
@@ -3,5 +3,29 @@
 from arithmetic import (add, subtract, multiply, divide, square, cube,
                         power, mod, )
 
+def run_prefix_calculator():
+    while True:
+        command_string = input(">\n").lower()
+        # print("str", command_string)
+        
+        command_list = command_string.split(" ")
+        # print("list", command_list)
+        operator = command_list[0]
 
-# Replace this with your code
+        if operator == "q":
+            break
+        
+
+        x = float(command_list[1])
+        y = float(command_list[2])
+
+        if operator == "+":
+            print(add(x,y))
+
+        elif operator == "-":
+            print(subtract(x,y))
+
+
+
+
+run_prefix_calculator()
